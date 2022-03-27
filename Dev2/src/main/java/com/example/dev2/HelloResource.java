dc959dff6506f2b0cab0527fb8a1c557d940f184
Marc Nseir <66441264+ElMaNs98@users.noreply.github.com>
2022-03-27 19:44:36 +0300

    Add files via upload

---
+++ Dev2/src/main/java/com/example/dev2/HelloResource.java
@@ -0,0 +1,17 @@
+package com.example.dev2;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ejb.*;
+
+@Stateless
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, My EJB";
+    }
+}
